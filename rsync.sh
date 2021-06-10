4217d1cb6092ce451cb37dc6d96c29c2a31501c2
Alexander Szczepanski <alexander@szczepan.ski>
2021-06-10 14:12:22 +0200

    added homeserver

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo rsync -ah --delete --progress /home/bernd/Workspace/nixos-config/ /root/nixos/
+sudo rsync -ah --delete --progress `pwd`/ /root/nixos/
 sudo rm /etc/nixos/configuration.nix
 sudo ln -s /root/nixos/machine/`hostname`/configuration.nix /etc/nixos/configuration.nix
 sudo nixos-rebuild switch
