fb0539016d4774ff34287a8bdaf07e5fa191f3bb
Alexander Szczepanski <alexander@szczepan.ski>
2021-08-01 18:40:42 +0200

    moved zsh to home-manger with p10k

--- configs/user.nix
+++ configs/user.nix
@@ -71,102 +71,44 @@
         };
       };
 
-      vscode = {
+      zsh = {
         enable = true;
-        package = pkgs.vscode;
-        extensions = with pkgs.vscode-extensions; [
-          bbenoist.Nix
-          justusadam.language-haskell
-          editorconfig.editorconfig
-          ms-azuretools.vscode-docker
-          #hsnazar.hyper-term-theme
-          #rafaelmaiolla.remote-vscode
-          eamodio.gitlens
+        enableAutosuggestions = true;
+        oh-my-zsh = {
+          enable = true;
+          plugins = [
+            "cp"
+            "common-aliases"
+            "docker "
+            "systemd"
+            "wd"
+            "kubectl"
+            "git"
+          ];
+          theme = "agnoster";
+        };
+        plugins = [
+          {
+            name = "powerlevel10k";
+            src = pkgs.zsh-powerlevel10k;
+            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+          }
+          {
+            name = "powerlevel10k-config";
+            src = lib.cleanSource ./p10k-config;
+            file = "p10k.zsh";
+          }
         ];
-        # userSettings = {
-        #   #"terminal.integrated.fontFamily" = "Hack";
-        #   "workbench.colorTheme" = "Hyper Term Black";
-        #   "window.titleBarStyle" = "custom";
-        # };
-      };
-
-      rofi = {
-        enable = true;
-        lines = 10;
-        borderWidth = 2;
-        scrollbar = false;
-        padding = 50;
-        font = "Liberation Sans Regular 20";
-        separator = "solid";
-        colors = {
-          window = {
-            background = "#dd000000";
-            border = "#a6a6a6";
-            separator = "#a6a6a6";
-          };
-          rows = {
-            normal = {
-              background = "#00000000";
-              foreground = "#a6a6a6";
-              backgroundAlt = "#00000000";
-              highlight = {
-                background = "#a6a6a6";
-                foreground = "#000000";
-              };
-            };
-          };
+        localVariables = {
+          EDITOR = "vim";
         };
-        extraConfig = {
-          modi = "drun,window";
-          show-icons = true;
+        shellAliases = {
+          lw = "lorri watch --once";
+          mff = "git merge --ff-only";
+          vi = "vim";
         };
-      };
-
-      mpv = {
-        enable = true;
-      };
-
-      kitty = {
-        enable = true;
-        extraConfig = ''
-            enabled_layouts splits:split_axis=vertical
-            enable_audio_bell false
-
-            map F5 launch --location=hsplit
-            map F6 launch --location=vsplit
-            map F7 layout_action rotate
-
-            map shift+up move_window up
-            map shift+left move_window left
-            map shift+right move_window right
-            map shift+down move_window down
-
-            map ctrl+left neighboring_window left
-            map ctrl+right neighboring_window right
-            map ctrl+up neighboring_window up
-            map ctrl+down neighboring_window down
-
-            background            #000000
-            foreground            #e9e9e9
-            cursor                #e9e9e9
-            selection_background  #424242
-            color0                #000000
-            color8                #000000
-            color1                #d44d53
-            color9                #d44d53
-            color2                #b9c949
-            color10               #b9c949
-            color3                #e6c446
-            color11               #e6c446
-            color4                #79a6da
-            color12               #79a6da
-            color5                #c396d7
-            color13               #c396d7
-            color6                #70c0b1
-            color14               #70c0b1
-            color7                #fffefe
-            color15               #fffefe
-            selection_foreground #000000
+        initExtra = ''
+          unset LESS
         '';
       };
     };
