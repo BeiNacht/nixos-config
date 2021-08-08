207946346bf4047c352855e5b8ea72596c265c0f
Alexander Szczepanski <alexander@szczepan.ski>
2021-08-08 15:28:46 +0200

    fixed some stuff around zsh

--- configs/user.nix
+++ configs/user.nix
@@ -9,17 +9,21 @@
     users.alex = {
       isNormalUser = true;
       extraGroups = [ "wheel" "docker" "networkmanager" "libvirtd" "lp" "scanner" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDPSzeNjfkz7/B/18TcJxxmNFUhvTKoieBcexdzebWH7oncvyBXNRJp8vAqSIVFLzz5UUFQNFuilggs8/N48U84acmFOxlbUmxlkf8KZgeB/G6uQ8ncQh6M1HNNPH+9apTURgfctr7eEZe9seLIEBISQLXB2Sf3F1ogfDj25S8kH9RM4wM1/jDFK5IecWHScKxwQPmCoXeGE1LEJq6nkQLXMDsWhSihtWouaTxSR0p7/wp/Rqt/hzLEWj8e3+qLMc5JrrdaWksupUCysme7CnSfGSzNUv9RKiRCTFofYPT9tbRn5JzdpQ55v22S6OvmmXUHjST1MOzI8MpVPZCCqd/ZQ1E+gErFiMwjG4sn/xxdPK9/jbQaXMjLklbKtR+C5090Ew2u2kj78jqGk/8COhF1MXh/9qjcG+C51uD1AS9d410kfjPwkaUt4U2KktDMQ942nWywrvIWM0Gt2kgDLYotsy/70q/aTJ8bvaCoWoDOGmpWcyNNBalz4OYYGI2Z0WHrVTs0FpzSk/XeQz0OLkmueoh5GDGd8zrfO6Nf5LWI17aWGRePTpQP5mJIg6jC3j8/QVrthEP6QyIIkZsnfsmvSiMWVfXqEy1BxVlu3T6aLffaj679KCsxY+mx5mTH2hwd4ZdbSI4F0GCIt+WGaFhHs2V3ZQitoEZuraRPEc4HGw== alexander@szczepan.ski"
+      ];
     };
   };
 
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
 
+  environment.pathsToLink = [ "/share/zsh" ];
+
   home-manager.users.alex = { pkgs, ... }: {
     home.enableNixpkgsReleaseCheck = false;
-    home.packages = [
-      pkgs.cryfs
-    ];
+    # home.packages =  with pkgs; [
+    # ];
 
     programs = {
       ssh = {
@@ -69,6 +73,8 @@
       zsh = {
         enable = true;
         enableAutosuggestions = true;
+        enableCompletion = true;
+        enableSyntaxHighlighting = true;
         oh-my-zsh = {
           enable = true;
           plugins = [
@@ -94,17 +100,32 @@
           }
         ];
         localVariables = {
-          SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+          #SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+
         };
         shellAliases = {
           active-services = "systemctl --no-page --no-legend --plain -t service --state=running";
           autofanspeed = "echo level auto | sudo tee /proc/acpi/ibm/fan";
           maxfanspeed = "echo level full-speed | sudo tee /proc/acpi/ibm/fan";
+          db = "sudo updatedb";
+          "-g C" = "| wc -l";
+          "-g G" = "| grep --ignore-case";
+          bat = "upower -i /org/freedesktop/UPower/devices/battery_BAT0";
+          # activate-smb="sc-start smb && sc-start nmb"
+          # stop-smb="sc-stop smb && sc-stop nmb"
+          brightness-max = "echo 4794 | sudo tee /sys/class/backlight/intel_backlight/brightness";
+          brightness-power-save = "echo 2300 | sudo tee /sys/class/backlight/intel_backlight/brightness";
+          ff = "find . -type f -iname";
+          l = "exa --group-directories-first -l -g";
+          ll = "exa --group-directories-first -l -g";
+          la = "exa --group-directories-first -l -g -a";
         };
-        initExtra = ''
-          eval $(/run/wrappers/bin/gnome-keyring-daemon --start --components=ssh)
-        '';
       };
+
+      # exa = {
+      #   enable = true;
+      #   enableAliases = true;
+      # };
     };
 
     # manuals not needed
