bf2632ef3f99357cb81756706773eb11f0d3e0df
Alexander Szczepanski <alexander@szczepan.ski>
2021-08-13 00:05:07 +0200

    added git secret

--- machine/vps.nix
+++ machine/vps.nix
@@ -30,13 +30,7 @@
     nodejs
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   security.acme.email = "webmaster@szczepan.ski";
   security.acme.acceptTerms = true;
