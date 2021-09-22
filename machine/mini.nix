2ff771a39e2319684451e0639cc1d78258ce6b09
Alexander Szczepanski <alexander@szczepan.ski>
2021-09-22 11:05:23 +0000

    added wifi for mini

--- machine/mini.nix
+++ machine/mini.nix
@@ -13,15 +13,21 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.extraModulePackages = with pkgs.linuxPackages; [ rtl88x2bu ];
 
-  networking.hostName = "mini";
-  networking.useDHCP = false;
-  networking.interfaces.enp3s0.useDHCP = true;
-  networking.firewall.allowedTCPPorts = [ 6443 ];
+
+  networking = {
+    hostName = "mini";
+    useDHCP = false;
+    # interfaces.enp3s0.useDHCP = true;
+    firewall = {
+      enable = false;
+      # allowedTCPPorts = [ 6443 ];
+    };
+    networkmanager.enable = true;
+  };
   services.k3s.enable = true;
   services.k3s.role = "server";
-
-  networking.firewall.enable = false;
 
   system.stateVersion = "21.05";
 }
