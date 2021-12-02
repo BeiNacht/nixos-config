ce294f2d3c3b16a3c1919ebe2d3f09e69e967f94
Alexander Szczepanski <alexander@szczepan.ski>
2021-12-02 08:42:29 +0100

    added etesync

--- configs/docker.nix
+++ configs/docker.nix
@@ -2,7 +2,10 @@
 
 {
   virtualisation = {
-    docker.enable = true;
+    docker = {
+      enable = true;
+      extraOptions = "--metrics-addr='127.0.0.1:9323' --experimental";
+    };
   };
 
   environment.systemPackages = with pkgs; [
