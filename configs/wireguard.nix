83648f860e70918eaa2c65850616dc9815ec4133
Alexander Szczepanski <alexander@szczepan.ski>
2023-10-22 13:43:21 +0200

    2023-10-22-13-43-21

--- configs/wireguard.nix
+++ configs/wireguard.nix
@@ -1,7 +1,7 @@
 {
   wireguard-desktop-public = "yQH91i1Kqxet5iGi3nFMgu7h68Oie6eHOQJHMkhLmWs=";
   wireguard-framework-public = "plnKdyh11Z3779LpHQmfU69z7LfCfDATLSxphhcj/3U=";
-  wireguard-mbp-public = "GQZ + lfEFQxTl5K5O623tlvWjTzBEeG0gSdvVA61H1z4=";
+  wireguard-mbp-public = "GQZ+lfEFQxTl5K5O623tlvWjTzBEeG0gSdvVA61H1z4=";
   wireguard-mini-public = "C9dfZUwmKLVpjeXVNkoVyMSXfogdqEK3yMgClpeYzHg=";
   wireguard-raspberrypi-public = "AB0k5pgI0fsnncNN3GkexWWbA8q/is9JaKY3x8jw4kY=";
   wireguard-vps-public = "gXG2XaFJZq3/MDva1vbaicRs9z9XfAR2wPLHmg4hRlI=";
